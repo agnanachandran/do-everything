e9db6515254ddd5caab78a536e882bd57295ba5b
Anojh Gnanachandran <anojhgnanachandran@gmail.com>
2014-05-22 16:49:08 -0400

    Begin to work with Yelp API. Prepare for transition to using Open Weather Map instead of Forecast.io

--- emotive/src/main/java/ca/pluszero/emotive/ApiKeys.java
+++ emotive/src/main/java/ca/pluszero/emotive/ApiKeys.java
@@ -6,4 +6,5 @@ public final class ApiKeys {
 	
 	public static final String GOOGLE_KEY = "AIzaSyB4rCEaU2X4yvhoHKMPqChMxRZl1PscKOs";
     public static final String FORECAST_KEY = "4f853b44ea54753921516c435ae0b498"; // https://developer.forecast.io/
+    public static final String OPEN_WEATHER_MAP_APP_ID_KEY = "fa4b4551e5d1bc38d9f7c76762e6750c"; // http://openweathermap.org/profile
 }
